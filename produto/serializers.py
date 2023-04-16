c724060fd4b3aa879ef12c70f3fbf340d97f90d2
vanessa cardoso <vanessacardosofigueira@gmail.com>
2023-04-16 19:26:02 -0300

    excecao serializer

--- produto/serializers.py
+++ produto/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 from produto.models import Produtos
 
@@ -6,3 +6,27 @@ class ProdutosSerializer(ModelSerializer):
     class Meta:
         model = Produtos
         fields = ["id", "nome", "valor"]
+
+#Se comecar com a letra maiusca retornar nome, caso nao retorna falso
+#Verificar se o nome ja existe, caso nao, que retorne falso
+
+    def validate_nome(self, nome):
+        if nome[0] == nome[0].title():
+            pass
+        else:
+            nome = nome.title()
+        if Produtos.objects.filter(nome= nome):
+            raise ValidationError("Já cadastrado!")
+        else:
+            return nome
+
+
+#O valor nao pode ser negativo, se for, retornar o int 0
+
+    def validate_valor(self,valor):
+        if valor > 0:
+            return valor
+        else:
+            return 0
+
+
